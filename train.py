d76ea46a8bd8def61fec0470cd285e6f1aa0e1cb
Tuhin Roy <81503907+TuhinRoy04@users.noreply.github.com>
2021-07-05 00:17:27 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import Sequential
+from keras.layers import Dense
+from sklearn import metrics
+import seaborn as sns
+import pandas as pd
+import numpy as np
+df=pd.read_csv('C:/Users/sayak/OneDrive/Documents/Projects/IITM_IPL_Hackathon/final_ipl_iit.csv')
+
+X=df.iloc[:,0:52].values
+y=df.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.20,random_state=50)
+
+model = Sequential()
+
+model.add(Dense(106, activation='relu'))
+model.add(Dense(212, activation='relu'))
+model.add(Dense(106, activation='relu'))
+model.add(Dense(53, activation='relu'))
+model.add(Dense(1))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+model.fit(
+    X_train,
+    y_train,
+    epochs=100,
+    shuffle=True,
+    verbose=2
+)
+
+prediction = model.predict(X_test)
+
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, prediction)))
